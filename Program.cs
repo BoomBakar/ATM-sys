fbee1ddc753c707ad8a6a12eadc6a22230b8961a
BoomBakar <abubakardar608@gmail.com>
2022-05-04 00:27:24 +0500

    ATM system added

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PresL;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM_View v = new ATM_View();
+            v.displayMenu();
+        }
+    }
+}
